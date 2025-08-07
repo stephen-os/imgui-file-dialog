69610f3dac97be2aa74f4c213ee200f1f6d2704d
stephen-os <ImStephen.1sw@gmail.com>
2025-08-06 20:01:20 -0400

    Updated premake5.lua.

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,10 @@
 project "ImGuiFD"
 	kind "StaticLib"
-	language "C++"
+	language "C++20"
     staticruntime "off"
+
+	-- Enable multi-core compilation
+	flags { "MultiProcessorCompile" }
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,29 +22,45 @@ project "ImGuiFD"
 
 	includedirs
 	{
-		"../imgui"
+		"../imgui",
+		"."
 	}
 
 	links { "ImGui" }
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
+	defines 
+    { 
+        "USE_THUMBNAILS",
+        "DONT_DEFINE_AGAIN__STB_IMAGE_IMPLEMENTATION"
+    } 
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
+ -- Platform-specific settings
+    filter "system:windows"
+        systemversion "latest"
+        
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        
+    filter "system:macosx"
+        systemversion "latest"
+        
+    -- Configuration-specific settings
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        defines { "DEBUG" }
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        defines { "NDEBUG" }
+        
     filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
+        defines { "NDEBUG", "DIST_BUILD" }
+        
+    -- Clear filters
+    filter {}
